a20483fb7b408a9b7a0083ff9a40006d3ed0b551
yfe404 <yfe.github@protonmail.com>
2025-05-01 17:05:18 +0200

    handle width and minus for i

--- ft_printf/ft_printf.c
+++ ft_printf/ft_printf.c
@@ -234,7 +234,19 @@ int ft_printf(const char * format, ...)
 							ft_putchar_fd(' ', 1);
 						}
 					}
+					int len = ft_count_digits_dec(arg) + flags.space * (arg >= 0); 
+					int delta = flags.width - len;
+					if ((delta > 0) && !flags.minus)
+					{
+						while (delta-- && ++count)
+							ft_putchar_fd(' ', 1);
+					}
 					ft_putnbr_fd(arg, 1);
+					if ((delta > 0) && flags.minus)
+					{
+						while (delta-- && ++count)
+							ft_putchar_fd(' ', 1);
+					}
 					count += ft_count_digits_dec(arg);
 					format += 1;
 				}

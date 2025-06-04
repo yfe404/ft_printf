a88a738b18ae1df158b34697e54263809d46cdd4
yfe404 <yfe.github@protonmail.com>
2025-06-04 23:13:45 +0200

    fix: precision / width for str

--- ft_printf/conversion/ft_string.c
+++ ft_printf/conversion/ft_string.c
@@ -6,7 +6,7 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 00:06:31 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/06/04 00:07:16 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/06/04 23:11:44 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,60 +34,36 @@ int	func_c(char arg, t_flags flags, int count)
 
 static int	__func_s_null(t_flags flags, int count)
 {
-	if (flags.width > 6)
-	{
-		count = flags.width - 6;
-		if (!flags.minus)
-			while (count--)
-				ft_putchar_fd(' ', 1);
-	}
+	if (flags.dot && flags.precision < 6)
+		return (0);
+	flags.width = max(0, flags.width - 6);
+	count += flags.width + 6;
+	if (flags.width && !flags.minus)
+		while (flags.width--)
+			ft_putchar_fd(' ', 1);
 	ft_putstr_fd("(null)", 1);
-	if (flags.width > 6)
-	{
-		count = flags.width - 6;
-		if (flags.minus)
-			while (count--)
-				ft_putchar_fd(' ', 1);
-	}
-	count += max(6, flags.width);
-	return (count);
-}
-
-static int	__func_s_dot(char *arg, t_flags flags, int count, int len)
-{
-	int	iter;
-	int	offset;
-
-	offset = 0;
-	iter = min(len, flags.precision);
-	count += min(len, flags.precision);
-	while (iter--)
-		ft_putchar_fd(arg[offset++], 1);
+	if (flags.width && flags.minus)
+		while (flags.width--)
+			ft_putchar_fd(' ', 1);
 	return (count);
 }
 
 int	func_s(char *arg, t_flags flags, int count)
 {
 	int	len;
-	int	delta;
 
-	len = 0;
 	if (arg == NULL)
 		return (__func_s_null(flags, count));
-	len = ft_strlen(arg);
-	delta = flags.width - len;
-	if ((delta > 0) && !flags.minus)
-		while (delta-- && ++count)
+	len = ft_strlen(arg) * !flags.dot + flags.dot \
+		* min(flags.precision, ft_strlen(arg));
+	count += max(flags.width, len);
+	flags.width = max(0, flags.width - len);
+	if (flags.width && !flags.minus)
+		while (flags.width--)
 			ft_putchar_fd(' ', 1);
-	if (flags.dot)
-		count = __func_s_dot(arg, flags, count, len);
-	else
-	{
-		ft_putstr_fd(arg, 1);
-		count += len;
-	}
-	if ((delta > 0) && flags.minus)
-		while (delta-- && ++count)
+	print_sub_str(arg, len);
+	if (flags.width && flags.minus)
+		while (flags.width--)
 			ft_putchar_fd(' ', 1);
 	return (count);
 }

51155e2bc220a986cae48114a33c8a8055803a3b
yfe404 <yfe.github@protonmail.com>
2025-06-04 23:13:08 +0200

    util: print n char from a str

--- ft_printf/utils.c
+++ ft_printf/utils.c
@@ -6,9 +6,11 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 22:38:28 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/06/03 22:38:58 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/06/04 23:02:07 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "libft.h"
 
 int	max(int a, int b)
 {
@@ -22,4 +24,12 @@ int	min(int a, int b)
 	if (a < b)
 		return (a);
 	return (b);
+}
+
+void	print_sub_str(const char *str, int n)
+{
+	if (n <= 0 || !str || !(*str))
+		return ;
+	while (n--)	
+		ft_putchar_fd(*str++, 1);
 }
--- ft_printf/utils.h
+++ ft_printf/utils.h
@@ -6,13 +6,14 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 22:39:30 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/06/03 22:40:10 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/06/04 23:03:43 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef UTILS_H
 # define UTILS_H
 
-int	max(int a, int b);
-int	min(int a, int b);
+int		max(int a, int b);
+int		min(int a, int b);
+void	print_sub_str(const char *str, int n);
 #endif

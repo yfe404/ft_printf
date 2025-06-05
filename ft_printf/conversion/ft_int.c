585af4120e23bcb1b9c77ff813a57732a7af7543
yfe404 <yfe.github@protonmail.com>
2025-06-05 11:32:06 +0200

    fix: conversion int

--- ft_printf/conversion/ft_int.c
+++ ft_printf/conversion/ft_int.c
@@ -6,7 +6,7 @@
 /*   By: yfeunteu <yfeunteu@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/03 23:26:15 by yfeunteu          #+#    #+#             */
-/*   Updated: 2025/06/03 23:26:15 by yfeunteu         ###   ########.fr       */
+/*   Updated: 2025/06/05 11:26:49 by yfeunteu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,81 @@
 #include "../parsing.h"
 #include "../numbers_utils_print.h"
 #include "../numbers_utils_count.h"
+#include "common.h"
 
-static char	get_prefix(t_flags flags)
+static int	get_sign(int number, t_flags flags, t_format *format)
 {
-	char	prefix;
-
-	if (flags.space || flags.plus)
+	if (number < 0)
 	{
-		prefix = flags.plus * '+';
-		prefix = prefix + !prefix * ' ';
+		format->sign_chr = '-';
+		return (1);
 	}
-	return (prefix);
+	if (flags.plus)
+	{
+		format->sign_chr = '+';
+		return (1);
+	}
+	if (flags.space)
+	{
+		format->sign_chr = ' ';
+		return (1);
+	}
+	return (0);
+}
+
+static int	zeros(int number, t_flags flags, t_format *format)
+{
+	int	digits_count;
+
+	if (!flags.dot)
+		return (0);
+	digits_count = ft_count_digits_dec(number);
+	format->n_zeros = max(0, flags.precision - digits_count);
+	return (format->n_zeros);
+}
+
+static int	padding(int num, t_flags flags, t_format *format)
+{
+	int	pad_width;
+
+	pad_width = max(0, flags.width - (format->sign_chr != 0) \
+	- format->n_zeros - ft_count_digits_dec(num));
+	if (flags.minus)
+		format->n_pad_right = pad_width;
+	else
+		format->n_pad_left = pad_width;
+	if (flags.zero && !(flags.dot || flags.minus))
+		format->pad_chr = '0';
+	else
+		format->pad_chr = ' ';
+	return (pad_width);
 }
 
 int	func_int(int arg, t_flags flags)
 {
-	char	prefix;
-	int		len;
-	int		delta;
+	int			count;
+	t_format	format;
 
-	prefix = get_prefix(flags);
-	flags.zero &= !(flags.dot || flags.minus);
-	if (flags.dot && (flags.precision > 0))
+	format = init_format();
+	if (arg == 0 && flags.precision == 0 && flags.dot)
 	{
-		flags.zero = 1;
-		flags.width = flags.precision + (arg < 0);
+		count = flags.width;
+		while (flags.width--)
+			ft_putchar_fd(' ', 1);
+		return (count);
 	}
-	len = ft_count_digits_dec(arg) + (flags.plus || flags.space) * (arg >= 0);
-	delta = flags.width - len;
-	if ((delta > 0) && !flags.minus && !flags.zero)
-		while (delta--)
-			ft_putchar_fd(' ', 1);
-	if ((flags.space || flags.plus) && (arg >= 0))
-		ft_putchar_fd(prefix, 1);
-	ft_putnbr_padding(arg, flags.zero * delta);
-	if ((delta > 0) && flags.minus && !flags.zero)
-		while (delta--)
-			ft_putchar_fd(' ', 1);
-	return (max(flags.width, len));
+	count = zeros(arg, flags, &format) + get_sign(arg, flags, &format);
+	count += padding(arg, flags, &format) + ft_count_digits_dec(arg);
+	while (format.pad_chr == ' ' && format.n_pad_left--)
+		ft_putchar_fd(' ', 1);
+	if (format.sign_chr)
+		ft_putchar_fd(format.sign_chr, 1);
+	while (format.pad_chr == '0' && format.n_pad_left--)
+		ft_putchar_fd('0', 1);
+	while (format.n_zeros--)
+		ft_putchar_fd('0', 1);
+	ft_putnbr_nosign_fd(arg, 1);
+	while (format.pad_chr == ' ' && format.n_pad_right--)
+		ft_putchar_fd(' ', 1);
+	return (count);
 }
